f423698a667d33589286e25bce9a4420a3d4b18d
Elgaml <mahmoudelgmal991@gmail.com>
2024-10-13 23:11:56 +0300

    -add jop_seeker_information page - make navigation to new screen

---
+++ app/src/main/java/com/example/myapplication/jop_seeker_information.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.FragmentJopSeekerInformationBinding
+
+
+class jop_seeker_information : Fragment() {
+    lateinit var binding: FragmentJopSeekerInformationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding =FragmentJopSeekerInformationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+
+}
